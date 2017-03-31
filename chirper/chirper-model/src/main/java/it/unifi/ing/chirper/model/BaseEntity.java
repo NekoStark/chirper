3058c031354a63271332dc10140c8931e553d7fd
stark <simone.mattolini@gmail.com>
2017-03-31 19:07:40 +0200

    more coverage and fix code smells

--- chirper/chirper-model/src/main/java/it/unifi/ing/chirper/model/BaseEntity.java
+++ chirper/chirper-model/src/main/java/it/unifi/ing/chirper/model/BaseEntity.java
@@ -38,7 +38,7 @@ public abstract class BaseEntity {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+		result = prime * result + uuid.hashCode();
 		return result;
 	}
 
